1b4b9846e8e032b59debe970bea290a6f2daf795
Wietse Wind <mail@wietse.com>
2021-10-26 15:45:41 +0200

    FH offloading (pool)

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ Proxy: offers rippled compatible JSON RPC endpoint, internally sending to WebSoc
 Runs under PM2 as `RPC2WS`;
 
 `npm run pm2` 
+
+# Environment Vars
+
+- Timeout (uplink WS response) in seconds: `TIMEOUT=60`
+- Debug (log) output: `DEBUG=post2ws*`
+- Non FH Query offloading (space separated): `NONFHNODES=ws://something:8080 ws://another:80`
--- index.js
+++ index.js
@@ -13,6 +13,39 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json({type: '*/*'}))
 
+const nonFhNodes = typeof process.env?.NONFHNODES === 'string' && process.env.NONFHNODES.trim() !== ''
+  ? (process.env?.NONFHNODES).replace(/[ \t]+/, ' ').trim().split(' ')
+  : []
+
+const nonFhCommand = data => {
+  // Mock to keep code below (edge) drop in compatible
+  const clientState = { uplinkType: 'mock' }
+
+  // For RPC, accept server_info to non-fh as well
+  if ((data.messageObject?.command || '').toLowerCase() === 'server_info') {
+    return true
+  }
+
+  // https://github.com/XRPLF/xrpl.ws-Edge-Proxy/blob/master/src/filtering/SubmitFilter.ts#L409
+  if (
+    (data.messageObject?.command || '').toLowerCase()
+      .match(/^(.*subscr.+|account_.+|ledger|ledger_cl.+|ledger_cu.+|book_of.+|deposit_auth.+|.*path_.+)$/)
+    && ([undefined, 'current', 'validated'].indexOf(data.messageObject?.ledger_index) > -1)
+    && (data.messageObject?.command.toLowerCase() !== 'account_tx')
+    && (typeof data.messageObject?.ledger_hash === 'undefined')
+    && (typeof data.messageObject?.ledger_index_min === 'undefined')
+    && (typeof data.messageObject?.ledger_index_max === 'undefined')
+    && (typeof data.messageObject?.forward === 'undefined')
+    && (typeof data.messageObject?.marker === 'undefined')
+    // Don't apply logic if connection is already of submit type (prevent endless recursion)
+    && clientState?.uplinkType !== 'submit' && clientState?.uplinkType !== 'nonfh'
+  ) {
+    return true
+  }
+
+  return false
+}
+
 const callTimeout = Number(process.env?.TIMEOUT || 60) || 60
 
 app.use((req, res, next) => {
@@ -31,11 +64,23 @@ app.post('/', async (req, res) => {
 
       logRequest(req.realip, req.callid, req.body.method.trim())
 
-      const connection = new XrplClient(null, {
-        assumeOfflineAfterSeconds: callTimeout,
-        maxConnectionAttempts: 2,
-        connectAttemptTimeoutSeconds: 4
-      })
+      const command = {
+        command: req.body.method,
+        ...req.body.params[0]
+      }
+
+      const nodes = nonFhCommand({messageObject: command})
+        ? nonFhNodes
+        : []
+
+      const connection = new XrplClient(
+        nodes.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value),
+        {
+          assumeOfflineAfterSeconds: callTimeout,
+          maxConnectionAttempts: 2,
+          connectAttemptTimeoutSeconds: 4
+        }
+      )
 
       connection.on('error', e => {
         if (!replied) {
@@ -47,11 +92,6 @@ app.post('/', async (req, res) => {
           })
         }
       })
-    
-      const command = {
-        command: req.body.method,
-        ...req.body.params[0]
-      }
       
       const timeout = setTimeout(() => {
         logTimeout(req.realip, req.callid, 'Timeout', JSON.stringify(command))
@@ -67,9 +107,19 @@ app.post('/', async (req, res) => {
 
       const result = await connection.send(command)
       clearTimeout(timeout)
+
+      if (command.command === 'server_info') {
+        if (result?.info?.complete_ledgers) {
+          Object.assign(result.info, {
+            complete_ledgers: '32570-' + result.info.complete_ledgers.split(',').reverse()[0].split('-').reverse()[0]
+          })
+        }
+      }
     
       const state = connection.getState()
       res.set('X-Cluster-Node', state.server.publicKey)
+      res.set('X-FH', nodes.length === 0 ? 1 : 0)
+
       logResponse(
         req.realip,
         req.callid,
